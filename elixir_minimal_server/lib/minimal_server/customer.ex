e7bdabd2949e078141e45a962a09ad760e103b9e
Pinho, Ágabo <agabo.pinho@outlook.com.br>
2019-12-08 21:49:05 -0300

    update elixir

--- elixir_minimal_server/lib/minimal_server/customer.ex
+++ elixir_minimal_server/lib/minimal_server/customer.ex
@@ -2,7 +2,6 @@ defmodule MinimalServer.Customer do
   use Plug.Router
 
   plug(:match)
-  plug(:dispatch)
 
   get "/" do
     conn
@@ -16,19 +15,9 @@ defmodule MinimalServer.Customer do
     )
   end
 
-  get "/1" do
-    conn
-    |> put_resp_content_type("application/json", "utf-8")
-    |> send_resp(
-      200,
-      Poison.encode!(%{
-        response_type: "in_channel",
-        text: "1-hello from BOT :)"
-      })
-    )
-  end
-
   match _ do
     send_resp(conn, 400, Poison.encode!(%{message: "not found customer"}))
   end
+
+  plug(:dispatch)
 end
